045e5725498ae8eadb103ee5700e721b9f2c86e3
Vansh Rathor <vanshrathor5887@gmail.com>
2026-07-07 23:40:18 +0530

    connect monogoDB to node

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const chats = require("./data/data");
-const dotenv=require("dotenv");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
 dotenv.config();
+connectDB();
+
 const app = express();
 
-app.get("/", (req, resp) => {
-  resp.end("Server is running");
+app.get("/", (req, res) => {
+  res.send("Server is running");
 });
 
-app.get("/api/chat", (req, resp) => {
-  resp.send(chats);
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
 });
-app.get("/api/chat/:id",(req,res)=>{
-    const singleChat=chats.find((c)=>c._id===req.params.id)
-    res.send(singleChat)
-})
 
-const PORT =process.env.PORT || 3000;
+app.get("/api/chat/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
