1b8a9cb60a0939423e0d44bb4e469333053f37f6
Vansh Rathor <vanshrathor5887@gmail.com>
2026-07-09 21:56:39 +0530

    feat:Auhorization middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express"); 
 const { registerUser } = require("../controller/userController");
 const { authUser } = require("../controller/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect,allUsers);
 router.route("/login").post(authUser); 
 
 module.exports = router;
