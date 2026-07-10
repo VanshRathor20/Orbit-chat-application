354532d5be0f08cecc168a742bf4cd1833c308f6
Vansh Rathor <vanshrathor5887@gmail.com>
2026-07-10 18:13:57 +0530

    feat(pages): add protected route logic and layout to ChatPage

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,22 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+import { Box, Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Chatbox from "../components/Chatbox";
+import MyChats from "../components/MyChats";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import { ChatState } from "../Context/ChatProvider";
 
-const ChatPage = () => {
-    const [chats, setChats] = useState([])
-    const fetchChats=async()=>{
-        const {data}=await axios.get("/api/chat")
-        setChats(data)
+const Chatpage = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
+  const { user } = ChatState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      navigate("/");
     }
-    useEffect(()=>{
-        fetchChats();
-    },[])
-  return (
-    <div>
-      {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-      ))}
-    </div>
-  );
-}
+  }, [user, navigate]);
 
-export default ChatPage
+  if (!user) {
+    return null;
+  }
+
+  return (
+    <Box w="100%">
+      <SideDrawer />
+      <Flex justifyContent="space-between" w="100%" h="91.5vh" p="10px" gap={4}>
+        <MyChats fetchAgain={fetchAgain} />
+        <Chatbox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+      </Flex>
+    </Box>
+  );
+};
+
+export default Chatpage;
